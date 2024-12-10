be28e27bbdb6b616f674b889d12cbe01a27ea45b
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2024-12-10 10:59:26 +0100

    Lessons 1.1a lights exercise

---
+++ Week 1/Lights Excersise 1.py
@@ -0,0 +1,12 @@
+from pyfirmata2 import Arduino, util
+import time
+
+board = Arduino('COM5')
+
+while True:
+    board.digital[7].write(1)
+    board.digital[6].write(0)
+    time.sleep(1)
+    board.digital[7].write(0)
+    board.digital[6].write(1)
+    time.sleep(1)
